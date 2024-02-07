29f6c7e9404647d52d3861b8316c22bd4d981631
Jerson La Torre <jerson.latorre@gmail.com>
2024-02-07 16:06:01 -0500

    uso del vh real

--- components/Mobile.vue
+++ components/Mobile.vue
@@ -6,15 +6,30 @@
   mobile-tag-list
   mobile-footer
 </template>
+
 <script>
-export default {}
+export default {
+  mounted() {
+    this.adjustHeight();
+    window.addEventListener('resize', this.adjustHeight);
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.adjustHeight);
+  },
+  methods: {
+    adjustHeight() {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+  }
+}
 </script>
 
 <style lang="sass" scoped>
 #mobile
   display: grid
   grid-template-rows: 50px 40vh auto 40vh 40px
-  height: 100vh
+  height: calc(var(--vh, 1vh) * 100) // Usa la variable CSS --vh para el alto
   width: 100%
   overflow: hidden
 </style>
